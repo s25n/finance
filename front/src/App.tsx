15814acbb83b7152448499d8d40765f915a42626
donguk.seo <donguk.seo@modori.io>
2021-12-03 09:00:55 +0900

    impl header

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Web3Context } from './providers/Web3Provider';
+import './App.css';
 import { VscGear } from 'react-icons/vsc';
 
 function App() {
@@ -9,48 +8,34 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <button
-          style={{
-            borderRadius: 25,
-            transition: "transform 0.2s ease 0s, box- shadow 0.2s ease 0s",
-            background: "rgb(248, 248, 248)",
-            border: "1px solid rgb(248, 248, 248)",
-            boxShadow: "rgb(233 233 233) 0px 0px 0px, rgb(255 255 255) 0px 0px 0px",
-            cursor: "pointer",
-            fontSize: 15,
-            padding: "5px 10px",
-            display: "flex",
-            alignItems: "center",
-          }}
-          onClick={connected ? reset : connect}
-        >
-          <div>
-            {
-              connected ? address && `${address.slice(0, 4)}...${address.slice(-4)}` : "Connect a wallet"
-            }
+      <div
+        className="Header"
+      >
+        <header>
+          <div className="Left">
+            <h1>s25n</h1>
           </div>
-          {
-            connected &&
-            <>
-              <div style={{ width: 20 }}></div>
-              <VscGear />
-            </>
-          }
-        </button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className="Right">
+            <button
+              className="Connect"
+              onClick={connected ? reset : connect}
+            >
+              <div>
+                {
+                  connected ? address && `${address.slice(0, 4)}...${address.slice(-4)}` : "Connect a wallet"
+                }
+              </div>
+              {
+                connected &&
+                <>
+                  <div style={{ width: 20 }}></div>
+                  <VscGear />
+                </>
+              }
+            </button>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
