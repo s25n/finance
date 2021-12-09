a52a56074a3e58be00ffc5c85c2a8490108f4868
donguk.seo <donguk.seo@modori.io>
2021-12-10 00:05:42 +0900

    remove networks

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -27,30 +27,8 @@ const config: HardhatUserConfig = {
     guardian: 3,
   },
   networks: {
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.ADMIN || ""],
-      chainId: 1,
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: {
-        mnemonic: process.env.TEST_MNEMONIC,
-      },
-      chainId: 3,
-    },
-    kovan: {
-      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: {
-        mnemonic: process.env.TEST_MNEMONIC,
-      },
-      chainId: 42,
-    },
     ganache: {
       url: "http://0.0.0.0:8545",
-      accounts: {
-        mnemonic: process.env.TEST_MNEMONIC,
-      },
     },
   },
 };
