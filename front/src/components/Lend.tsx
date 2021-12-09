9dafe8fc284a029c305a73afaae27bd01a5599dc
hyemmie <lhm0710@naver.com>
2021-12-09 19:46:38 +0900

    feat lend, pool box ui before connect wallet

--- front/src/components/Lend.tsx
+++ front/src/components/Lend.tsx
@@ -1,9 +1,95 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Web3Context } from "../providers/Web3Provider";
+import layerIcon from "../assets/images/layers.png";
+import daiIcon from "../assets/images/dai.png";
+import "./css/modeBox.css";
 
 interface Props {}
 
 const Lend = (props: Props) => {
-  return <div></div>;
+  const { connected, address, reset, connect } = useContext(Web3Context);
+  const confirmButtonHandler = () => {
+    if (!connected) {
+      connect();
+    } else {
+    }
+  };
+
+  return (
+    <div className="mode-container">
+      <div className="mode-color-box">
+        <div className="mode-color-box-contents">
+          <div className="mode-color-box-series">
+            <div className="mode-series">
+              <div className="series-apy">
+                <span style={{ fontSize: 30 }}>7.00%</span>
+              </div>
+              <span
+                style={{
+                  fontFamily: "LexendDeca",
+                  top: 0,
+                  height: 30,
+                  fontSize: 20,
+                  color: "#343434",
+                  flex: 3,
+                }}
+              >
+                December 2021
+              </span>
+            </div>
+            <div style={{ width: 100, minWidth: 30 }} />
+            <button className="button-series">
+              <div className="button-series-content">
+                <img src={layerIcon} alt="" width="15" height="15" />
+                <span
+                  style={{
+                    alignSelf: "center",
+                    textAlign: "center",
+                    verticalAlign: "center",
+                    top: 0,
+                    height: 30,
+                    fontSize: 13,
+                    lineHeight: 2,
+                    color: "#343434",
+                  }}
+                >
+                  Change Series
+                </span>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="mode-detail-box">
+        <span
+          style={{
+            fontFamily: "LexendDeca",
+            top: 0,
+            height: 30,
+            fontSize: 20,
+            color: "#343434",
+            flex: 3,
+            textAlign: "left",
+          }}
+        >
+          Amount to lend
+        </span>
+        <div className="lend-input-box">
+          <div style={{ padding: 5 }}>
+            <img src={daiIcon} alt={"dai"} width={20} height={20} />
+          </div>
+          <input
+            className="input-lend-amount"
+            placeholder={"Enter the amount of DAI to lend"}
+            disabled={!connected}
+          ></input>
+        </div>
+        <button className="button-submit" onClick={confirmButtonHandler}>
+          {connected ? "Confirm" : "Connet Wallet"}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Lend;
